f566ee1750ce3240c16be1c638372343033d1ce2
Greg Tatum <tatum.creative@gmail.com>
2020-09-13 10:00:26 -0500

    Clear the screen for console messages

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,34 +1,34 @@
 #!/usr/bin/env node
 
 /**
- * This file is in charge of live-reloading the example. It is used by the make file
- * via `make watch EXAMPLE=bunny`.
+ * This file is in charge of live-reloading the example. It is used by the make
+ * file via `make watch EXAMPLE=bunny`.
  */
 
-const watch = require("watch");
-const path = require("path");
-const { spawn, spawnSync, exec, execSync } = require("child_process");
-const rootPath = path.join(__dirname, "..");
+const watch = require('watch');
+const path = require('path');
+const {spawn, spawnSync, exec, execSync} = require('child_process');
+const rootPath = path.join(__dirname, '..');
 const example = getExample();
 let exampleSubProcess;
 let isClosingSubProcess = false;
 
 const metalValidation = {
   // Enables all shader validation tests.
-  MTL_SHADER_VALIDATION: "1",
+  MTL_SHADER_VALIDATION: '1',
   // Validates accesses to device and constant memory.
-  MTL_SHADER_VALIDATION_GLOBAL_MEMORY: "1",
+  MTL_SHADER_VALIDATION_GLOBAL_MEMORY: '1',
   // Validates accesses to threadgroup memory.
-  MTL_SHADER_VALIDATION_THREADGROUP_MEMORY: "1",
+  MTL_SHADER_VALIDATION_THREADGROUP_MEMORY: '1',
   // Validates that texture references are not nil.
-  MTL_SHADER_VALIDATION_TEXTURE_USAGE: "1",
+  MTL_SHADER_VALIDATION_TEXTURE_USAGE: '1',
 };
 
 buildAndRun();
 watchFiles();
 
-process.on("SIGINT", function () {
-  console.log("Closing the example");
+process.on('SIGINT', function() {
+  console.log('Closing the example');
   exampleSubProcess.kill();
   process.exit(0);
 });
@@ -40,11 +40,11 @@ function buildAndRun() {
 }
 
 function build() {
-  console.log("🛠  Building " + example);
+  console.log('🛠  Building ' + example);
   try {
-    execSync(`make ./bin/${example}`, { cwd: rootPath, stdio: "inherit" });
+    execSync(`make ./bin/${example}`, {cwd: rootPath, stdio: 'inherit'});
   } catch (error) {
-    console.error("🛑 Could not build " + example);
+    console.error('🛑 Could not build ' + example);
     return false;
   }
   return true;
@@ -54,7 +54,7 @@ function closeSubprocess() {
   isClosingSubProcess = true;
   const success = exampleSubProcess.kill();
   if (!success) {
-    console.error("Unable to close the example " + example);
+    console.error('Unable to close the example ' + example);
     process.exit(1);
   }
 }
@@ -62,23 +62,22 @@ function closeSubprocess() {
 function runExample() {
   if (exampleSubProcess) {
     throw new Error(
-      "A subprocess was still running when launching another example."
-    );
+        'A subprocess was still running when launching another example.');
   }
-  console.log("🚂 Running " + example);
+  console.log('🚂 Running ' + example);
   const args = [];
   exampleSubProcess = spawn(`./bin/${example}`, args, {
     cwd: rootPath,
     detached: true,
-    stdio: "inherit",
+    stdio: 'inherit',
     env: metalValidation,
   });
 
-  exampleSubProcess.on("close", (code, signal) => {
+  exampleSubProcess.on('close', (code, signal) => {
     if (isClosingSubProcess) {
       isClosingSubProcess = false;
     } else {
-      console.log("Example closed, stopped watching for changes.");
+      console.log('Example closed, stopped watching for changes.');
       process.exit(0);
     }
   });
@@ -110,7 +109,8 @@ function handleFileChange(...args) {
     // This is the first run, do nothing.
   } else {
     const [fileName, prevStat, currState] = args;
-    console.log("🙈 File change detected", fileName);
+    console.clear();
+    console.log('🙈 File change detected', fileName);
     if (exampleSubProcess) {
       closeSubprocess();
       exampleSubProcess = null;
@@ -120,35 +120,32 @@ function handleFileChange(...args) {
 }
 
 function watchFiles() {
-  const paths = [
-    path.join(__dirname, "../src"),
-    path.join(__dirname, "../examples"),
-  ];
+  const paths = [path.join(__dirname, '../src')];
   for (const path of paths) {
     // Interval is in seconds.
-    const options = { interval: 0.5 };
+    const options = {interval: 0.5};
     watch.watchTree(path, options, handleFileChange);
   }
-  console.log("👀 Watching the tree");
+  console.log('👀 Watching the tree');
 }
 
 function getExample() {
   const example = process.argv[2];
 
   function error(msg) {
-    console.error("Error: " + msg);
-    console.error("Usage: ./scripts/watch.js bunny");
+    console.error('Error: ' + msg);
+    console.error('Usage: ./scripts/watch.js bunny');
     process.exit(1);
   }
 
   if (!example) {
-    error("No example given to be built.");
+    error('No example given to be built.');
   }
   if (process.argv.length !== 3) {
-    error("Only one argument may be given to this command.");
+    error('Only one argument may be given to this command.');
   }
   if (!example.match(/^[\w-_]+$/)) {
-    error("Examples should only be alphanumeric with dashes or underscores.");
+    error('Examples should only be alphanumeric with dashes or underscores.');
   }
   return example;
 }
