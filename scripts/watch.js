7ce3e55f52aa95099d0726fc04b0e9e5436c53e9
Greg Tatum <tatum.creative@gmail.com>
2020-09-16 06:15:58 -0500

    I guess this is saving with double quotes now?

--- scripts/watch.js
+++ scripts/watch.js
@@ -5,23 +5,23 @@
  * file via `make watch EXAMPLE=bunny`.
  */
 
-const watch = require('watch');
-const path = require('path');
-const {spawn, spawnSync, exec, execSync} = require('child_process');
-const rootPath = path.join(__dirname, '..');
+const watch = require("watch");
+const path = require("path");
+const { spawn, spawnSync, exec, execSync } = require("child_process");
+const rootPath = path.join(__dirname, "..");
 const example = getExample();
 let exampleSubProcess;
 let isClosingSubProcess = false;
 
 const metalValidation = {
   // Enables all shader validation tests.
-  MTL_SHADER_VALIDATION: '1',
+  MTL_SHADER_VALIDATION: "1",
   // Validates accesses to device and constant memory.
-  MTL_SHADER_VALIDATION_GLOBAL_MEMORY: '1',
+  MTL_SHADER_VALIDATION_GLOBAL_MEMORY: "1",
   // Validates accesses to threadgroup memory.
-  MTL_SHADER_VALIDATION_THREADGROUP_MEMORY: '1',
+  MTL_SHADER_VALIDATION_THREADGROUP_MEMORY: "1",
   // Validates that texture references are not nil.
-  MTL_SHADER_VALIDATION_TEXTURE_USAGE: '1',
+  MTL_SHADER_VALIDATION_TEXTURE_USAGE: "1",
 };
 
 clearConsole();
@@ -29,8 +29,8 @@ buildAndRun();
 watchFiles();
 listenToKeyboard();
 
-process.on('SIGINT', function() {
-  console.log('Closing the example');
+process.on("SIGINT", function () {
+  console.log("Closing the example");
   exampleSubProcess.kill();
   process.exit(0);
 });
@@ -42,11 +42,11 @@ function buildAndRun() {
 }
 
 function build() {
-  console.log('🛠  Building ' + example);
+  console.log("🛠  Building " + example);
   try {
-    execSync(`make ./bin/${example}`, {cwd: rootPath, stdio: 'inherit'});
+    execSync(`make ./bin/${example}`, { cwd: rootPath, stdio: "inherit" });
   } catch (error) {
-    console.error('🛑 Could not build ' + example);
+    console.error("🛑 Could not build " + example);
     return false;
   }
   return true;
@@ -56,7 +56,7 @@ function closeSubprocess() {
   isClosingSubProcess = true;
   const success = exampleSubProcess.kill();
   if (!success) {
-    console.error('Unable to close the example ' + example);
+    console.error("Unable to close the example " + example);
     process.exit(1);
   }
   exampleSubProcess = null;
@@ -65,74 +65,75 @@ function closeSubprocess() {
 function listenToKeyboard() {
   process.stdin.setRawMode(true);
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', function(key) {
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", function (key) {
     switch (key) {
-      case 'c':
+      case "c":
         try {
           clearConsole();
-          console.log('🧹 Cleaning all of the C++ files.');
-          console.log('');
-          execSync(`make clean-cpp`, {cwd: rootPath, stdio: 'inherit'});
+          console.log("🧹 Cleaning all of the C++ files.");
+          console.log("");
+          execSync(`make clean-cpp`, { cwd: rootPath, stdio: "inherit" });
         } catch (error) {
-          console.error('🛑 Could not clean the C++ files. ' + example);
+          console.error("🛑 Could not clean the C++ files. " + example);
           return false;
         }
         closeAndRebuild();
         break;
-      case 'a':
+      case "a":
         clearConsole();
-        console.log('🧹 It\'s time for a fresh start');
-        console.log('');
+        console.log("🧹 It's time for a fresh start");
+        console.log("");
         try {
-          execSync(`make clean`, {cwd: rootPath, stdio: 'inherit'});
+          execSync(`make clean`, { cwd: rootPath, stdio: "inherit" });
         } catch (error) {
-          console.error('🛑 Could not clean the files. ' + example);
+          console.error("🛑 Could not clean the files. " + example);
           return false;
         }
         closeAndRebuild();
         break;
-      case 'r':
+      case "r":
         closeSubprocess();
         clearConsole();
         runExample();
         break;
-      case 'q':
-      case '\u0003':
+      case "q":
+      case "\u0003":
         closeSubprocess();
         process.exit();
     }
   });
-  console.log('');
-  console.log('-------------------------');
-  console.log('| Keyboard shortcuts:   |');
-  console.log('-------------------------');
-  console.log('  c - Clean the C++ files');
-  console.log('  a - Clean all the files');
-  console.log('  r - Restart the example');
-  console.log('  q - Quit');
-  console.log('');
+  console.log("");
+  console.log("-------------------------");
+  console.log("| Keyboard shortcuts:   |");
+  console.log("-------------------------");
+  console.log("  c - Clean the C++ files");
+  console.log("  a - Clean all the files");
+  console.log("  r - Restart the example");
+  console.log("  q - Quit");
+  console.log("");
 }
 
 function runExample() {
   if (exampleSubProcess) {
     throw new Error(
-        'A subprocess was still running when launching another example.');
+      "A subprocess was still running when launching another example."
+    );
   }
-  console.log('🚂 Running ' + example);
+  console.log("🚂 Running " + example);
   const args = [];
   exampleSubProcess = spawn(`./bin/${example}`, args, {
     cwd: rootPath,
     detached: true,
-    stdio: 'inherit',
+    stdio: "inherit",
     env: metalValidation,
   });
 
-  exampleSubProcess.on('close', (code, signal) => {
+  exampleSubProcess.on("close", (code, signal) => {
     if (isClosingSubProcess) {
       isClosingSubProcess = false;
     } else {
-      console.log('Example closed, stopped watching for changes.');
+      console.log("Example closed, stopped watching for changes.");
       process.exit(0);
     }
   });
@@ -165,7 +166,7 @@ function handleFileChange(...args) {
   } else {
     const [fileName, prevStat, currState] = args;
     clearConsole();
-    console.log('🙈 File change detected', fileName);
+    console.log("🙈 File change detected", fileName);
     closeAndRebuild();
   }
 }
@@ -178,38 +179,38 @@ function closeAndRebuild() {
 }
 
 function watchFiles() {
-  const paths = [path.join(__dirname, '../src')];
+  const paths = [path.join(__dirname, "../src")];
   for (const path of paths) {
     // Interval is in seconds.
-    const options = {interval: 0.5};
+    const options = { interval: 0.5 };
     watch.watchTree(path, options, handleFileChange);
   }
-  console.log('👀 Watching the tree');
+  console.log("👀 Watching the tree");
 }
 
 function getExample() {
   const example = process.argv[2];
 
   function error(msg) {
-    console.error('Error: ' + msg);
-    console.error('Usage: ./scripts/watch.js bunny');
+    console.error("Error: " + msg);
+    console.error("Usage: ./scripts/watch.js bunny");
     process.exit(1);
   }
 
   if (!example) {
-    error('No example given to be built.');
+    error("No example given to be built.");
   }
   if (process.argv.length !== 3) {
-    error('Only one argument may be given to this command.');
+    error("Only one argument may be given to this command.");
   }
   if (!example.match(/^[\w-_]+$/)) {
-    error('Examples should only be alphanumeric with dashes or underscores.');
+    error("Examples should only be alphanumeric with dashes or underscores.");
   }
   return example;
 }
 
 function clearConsole() {
-  process.stdout.write('\033[2J\033[3J\033[1;1H');
+  process.stdout.write("\033[2J\033[3J\033[1;1H");
   // process.stdout.write('\x1Bc');
   // console.log('\033]50;ClearScrollback\a');
   // printf '\033[2J\033[3J\033[1;1H'
